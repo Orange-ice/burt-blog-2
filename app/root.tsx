e5bc75b06b93f411fa50980e636195ed5712a53e
Burt <2902978956@qq.com>
2022-07-17 21:10:37 +0800

    添加 global css

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {LinksFunction, MetaFunction} from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -6,27 +6,32 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
+import globalStyleUrl from '~/styles/global.css';
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'Burt Blog',
+  viewport: 'width=device-width,initial-scale=1',
 });
+
+export const links: LinksFunction = () => [
+  {rel: 'stylesheet', href: globalStyleUrl},
+];
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+    <html lang="zh">
+    <head>
+      <Meta />
+      <Links />
+    </head>
+    <body>
+    <Outlet />
+    <ScrollRestoration />
+    <Scripts />
+    <LiveReload />
+    </body>
     </html>
   );
 }
