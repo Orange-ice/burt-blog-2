fa474fee13cc2d37d35b6d3eb2a563759942612b
Burt <2902978956@qq.com>
2022-07-23 21:46:05 +0800

    mock user 数据

---
+++ prisma/seed.ts
@@ -0,0 +1,15 @@
+import {PrismaClient} from '@prisma/client';
+
+const db = new PrismaClient();
+
+async function seed() {
+  await db.user.create({
+    data: {
+      name: 'Burt',
+      email: '2902978956@qq.com',
+      passwordHash: '$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u',
+    }
+  });
+}
+
+seed();
