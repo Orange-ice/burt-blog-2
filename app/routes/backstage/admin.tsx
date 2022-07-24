4b5d89628ebce19d8d9ccaaf02c576c0fff8db28
Burt <2902978956@qq.com>
2022-07-24 13:34:42 +0800

    admin页面添加部分样式

--- app/routes/backstage/admin.tsx
+++ app/routes/backstage/admin.tsx
@@ -1,9 +1,14 @@
 /**
  * @description 后台主页
  * */
-import {Outlet} from '@remix-run/react';
-import type {LoaderFunction} from '@remix-run/node';
+import {Link, Outlet} from '@remix-run/react';
+import type {LinksFunction, LoaderFunction} from '@remix-run/node';
 import {requireUserId} from '~/utils/session.server';
+import adminStyleUrl from '~/styles/backstage/admin.css';
+
+export const links: LinksFunction = () => [
+  {rel: 'stylesheet', href: adminStyleUrl},
+];
 
 export const loader: LoaderFunction = async ({request}) => {
   const userId = await requireUserId(request);
@@ -13,12 +18,29 @@ export const loader: LoaderFunction = async ({request}) => {
 
 export default function BackstageAdmin() {
   return (
-    <div>
-      <aside>aside</aside>
-      <main>
-        <header>header</header>
-        <Outlet />
-      </main>
+    <div className="container">
+      <header>
+        <h1>Burt Blog 后台系统</h1>
+        <span>welcome, username!</span>
+        <button>退出</button>
+      </header>
+      <div className="content">
+        <aside>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/backstage/admin">首页</Link>
+              </li>
+              <li>
+                <Link to="posts">博客管理</Link>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ app/routes/backstage/admin/index.tsx
@@ -0,0 +1,5 @@
+export default function AdminIndex() {
+  return (
+    <div>Index Page</div>
+  );
+}
